51a6bfd677845d9bc7ad16eac999aa913f040a86
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-06-19 20:57:52 -0600

    up to excited kitten

---
+++ js/app.js
@@ -0,0 +1,34 @@
+//console.log("going!");
+
+
+//Easy Going
+//1.
+//for (let i = 1; i <20; i++){
+//	console.log("Commit 1 - Easy Going answered");
+//}
+
+//Get Even
+for (let i = 0; i <200; i++){
+	if (i%2 === 0){
+	console.log(`Commit 2 - Get even answered ${i}`);
+	}
+}
+
+//Excited Kitten
+//1.
+//for (let i = 0; i <20; i++){
+//	console.log(`Love me, pet me! HSSSSSS! ${i}`);
+//	if (i%2 === 0){
+//		var msg =[
+//		"...human...why you taking pictures of me?...",
+//		"...the catnip made me do it...",
+//		"...why does the red dot always get away..."
+//		];
+//
+//		function randomMsg (){
+//			var random = Math.floor(Math.random() * 3);
+//			alert(msg[random]);
+//		}
+//		//console.log("...the catnip made me do it...")
+//	}
+//}
