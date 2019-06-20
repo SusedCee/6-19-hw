c0ba0ddd7a82daf6aa1e8b576104edfaabc3e9b0
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-06-19 22:26:55 -0600

    Commit 5 - Getting to Know You answered

--- js/app.js
+++ js/app.js
@@ -34,18 +34,51 @@
 
 //FizzBuzz
 //1-5
-for (let i = 1; i <= 100; i++){
-		console.log(`${i}`);     // logs all numbers 1-100
-	if(i%3 === 0){
-		console.log("Fizz");
-	}
-	if(i%5 === 0){
-		console.log("Buzz");
-	}
-	if(i%3 === 0 && i%5 === 0){
-		console.log("FizzBuzz");
-	}
-}
+//for (let i = 1; i <= 100; i++){
+//		console.log(`${i}`);     // logs all numbers 1-100
+//	if(i%3 === 0){
+//		console.log("Fizz");
+//	}
+//	if(i%5 === 0){
+//		console.log("Buzz");
+//	}
+//	if(i%3 === 0 && i%5 === 0){
+//		console.log("FizzBuzz");
+//	}
+//}
+
+//Getting to Know You
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+//1.
+kenny.shift();
+kenny.unshift("Gameboy");
+console.log(kenny);
+
+//2.
+jimClark[1] = jimClark[1] + 1;
+console.log(jimClark);
+
+//3.
+ryan.pop();
+ryan.push("Gotham City")
+console.log(ryan);
+
+//4.
+reuben.pop();
+reuben.push("Chicago");
+console.log(reuben);
+
+//5.
+jimHaff.pop();
+jimHaff.push('Dallas', 'Portland', 'Anchorage');
+console.log(jimHaff);
+
+
 
 
 
