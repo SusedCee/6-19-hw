5df102e1f66903b34766c9173d6ba0fce1eb4ade
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-06-20 17:06:58 -0600

    no commnets

--- js/app.js
+++ js/app.js
@@ -3,48 +3,48 @@
 
 //Easy Going
 //1.
-//for (let i = 1; i <20; i++){
+for (let i = 1; i <20; i++){
 
 
 //Get Even
-//for (let i = 0; i <200; i++){
-//	if (i%2 === 0)
-//}
+for (let i = 0; i <200; i++){
+	if (i%2 === 0)
+}
 
 //Excited Kitten
 //1.
-//for (let i = 0; i <20; i++){
+for (let i = 0; i <20; i++){
+}
 
 //2.
-//	console.log(`Love me, pet me! HSSSSSS! ${i}`); // logs the message 20 times
-//		if (i%2 === 0){
-//			function random_item(message).    // more info about this at: https://www.w3resource.com/javascript-exercises/javascript-array-exercise-35.php
-//			{
-//
-//			return message[Math.floor(Math.random()*message.length)];
-//			}
-//			var message = ["...human...why you taking pictures of me?...", 
-//			"...the catnip made me do it...", "...why does the red dot always get away..."]
-//		console.log(random_item(message));
-//		}
-//
-//}
+	console.log(`Love me, pet me! HSSSSSS! ${i}`); // logs the message 20 times
+		if (i%2 === 0){
+			function random_item(message).    // more info about this at: https://www.w3resource.com/javascript-exercises/javascript-array-exercise-35.php
+			{
+
+			return message[Math.floor(Math.random()*message.length)];
+			}
+			var message = ["...human...why you taking pictures of me?...", 
+			"...the catnip made me do it...", "...why does the red dot always get away..."]
+		console.log(random_item(message));
+		}
+
+}
 
 
-//	if (i%2 === 0){       //if i is an even number...
-//		var msg =[        // a random message 1 of 3 is displayed
-//		"...human...why you taking pictures of me?...",
-//		"...the catnip made me do it...",
-//		"...why does the red dot always get away..."
-//		];
-//
-//		function randomMsg (){
-//			var random = Math.floor(Math.random() * 3);
-//			alert(msg[random]);
-//		}
-//		//console.log("...the catnip made me do it...")
-//	}
-//}
+	if (i%2 === 0){       //if i is an even number...
+		var msg =[        // a random message 1 of 3 is displayed
+		"...human...why you taking pictures of me?...",
+		"...the catnip made me do it...",
+		"...why does the red dot always get away..."
+		];
+
+		function randomMsg (){
+			var random = Math.floor(Math.random() * 3);
+			alert(msg[random]);
+		}
+		console.log(randomMessage);
+	}
 
 //FizzBuzz
 //1-5
@@ -62,87 +62,87 @@ for (let i = 1; i <= 100; i++){
 }
 
 //Getting to Know You
-//const kenny = ["Kenny", 1000, "Austin"];
-//const jimHaff = ["Jim H", 16, "All cities"];
-//const reuben = ["Reuben", 22, "Durham"];
-//const jimClark = ["Jim C", 186, "LA"];
-//const ryan = ["Ryan", 65, "Denver"];
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
 
 //1.
-//kenny.shift();
-//kenny.unshift("Gameboy");
-//console.log(kenny);
+kenny.shift();
+kenny.unshift("Gameboy");
+console.log(kenny);
 
 //2.
-//jimClark[1] = jimClark[1] + 1;
-//console.log(jimClark);
+jimClark[1] = jimClark[1] + 1;
+console.log(jimClark);
 
 //3.
-//ryan.pop();
-//ryan.push("Gotham City")
-//console.log(ryan);
+ryan.pop();
+ryan.push("Gotham City")
+console.log(ryan);
 
 //4.
-//reuben.pop();
-//reuben.push("Chicago");
-//console.log(reuben);
+reuben.pop();
+reuben.push("Chicago");
+console.log(reuben);
 
 //5.
-//jimHaff.pop();
-//jimHaff.push('Dallas', 'Portland', 'Anchorage');
-//console.log(jimHaff);
+jimHaff.pop();
+jimHaff.push('Dallas', 'Portland', 'Anchorage');
+console.log(jimHaff);
 
 
 
 //Yell at the Ninja Turtles
 //1.
-//const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
 //2. 
-//for(var i = 0; i < ninjaTurtles.length; i++){
-//   ninjaTurtles[i] = ninjaTurtles[i].toUpperCase();
-//   console.log(ninjaTurtles);
-//}
+for(var i = 0; i < ninjaTurtles.length; i++){
+  ninjaTurtles[i] = ninjaTurtles[i].toUpperCase();
+  console.log(ninjaTurtles);
+}
 
 
 
 // Return to Closets
-//const kristynsCloset = [
-//  "left shoe",
-//  "cowboy boots",
-//  "right sock",
-//  "GA hoodie",
-//  "green pants",
-//  "yellow knit hat",
-//  "marshmallow peeps"
-//];
+const kristynsCloset = [
+ "left shoe",
+ "cowboy boots",
+ "right sock",
+ "GA hoodie",
+ "green pants",
+ "yellow knit hat",
+ "marshmallow peeps"
+];
 
 // Thom's closet is more complicated. Check out this nested data structure!!
-//const thomsCloset = [
-//  [
-   // These are Thom's shirts
-//    "grey button-up",
-//    "dark grey button-up",
-//    "light blue button-up",
-//    "blue button-up",
-//  ],[
-//   // These are Thom's pants
-//    "grey jeans",
-//    "jeans",
-//    "PJs"
-//  ],[
-//    // Thom's accessories
-//    "wool mittens",
-//    "wool scarf",
-//    "raybans"
-//  ]
-//];
+const thomsCloset = [
+ [
+   //These are Thom's shirts
+   "grey button-up",
+   "dark grey button-up",
+   "light blue button-up",
+   "blue button-up",
+ ],[
+  // These are Thom's pants
+   "grey jeans",
+   "jeans",
+   "PJs"
+ ],[
+   // Thom's accessories
+   "wool mittens",
+   "wool scarf",
+   "raybans"
+ ]
+];
 
 
 //Dirty Laundry
 //1.
-// for (i = 0; i <kristynsCloset.length; i++){
-// 	console.log(`WHIRR: Now washing ${i}`);
-// }
+for (i = 0; i <kristynsCloset.length; i++){
+	console.log(`WHIRR: Now washing ${i}`);
+}
 // 2. Still need number 2
 
 
@@ -151,20 +151,25 @@ for (let i = 1; i <= 100; i++){
 
 //Multiples of 3 and 5 
 // More info https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2
-//function sumOfMultiples(number){
-//	let sum = 0
+function sumOfMultiples(number){
+	let sum = 0
 
-//	for (i = 1; i <1000; i++){
-//		if (i%3 === 0 || i%5 === 0){
-//			sum += i;
-//		}
-//	}
-//	return sum;
-//}
-//console.log(sumOfMultiples(5));
-//console.log(sumOfMultiples(3));
+	for (i = 1; i <1000; i++){
+		if (i%3 === 0 || i%5 === 0){
+			sum += i;
+		}
+	
+	return sum;
+	}
+console.log(sumOfMultiples(5));
+console.log(sumOfMultiples(3));
 
 	
+
+
+
+
+
 
 
 
